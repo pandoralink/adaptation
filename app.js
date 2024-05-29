67b6c6375084792c3fee56a6ac11e53ba11aef70
pandoralink <2593876610@qq.com>
2024-05-29 23:08:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import { Gallery } from './image-list.js';
 
+const foo = ()=> {
+  const search = window.location.search;
+  const searchParam = new URLSearchParams(search.split('?')[1] || "");
+  const addParam = searchParam.get("add");
+  if(addParam!==null) {
+    const add = localStorage.getItem("add") || 0;
+    localStorage.setItem("add",Number(add)+ 1);
+  }
+}
+foo();
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
@@ -58,3 +68,23 @@ const createGalleryFigure = async (galleryImage) => {
 
 registerServiceWorker();
 Gallery.images.map(createGalleryFigure);
+// 从localStorage中获取键为'add'的值
+var itemValue = localStorage.getItem('add');
+
+// 验证是否成功获取了值
+if (itemValue) {
+    // 创建一个新的div元素
+    var newDiv = document.createElement('div');
+
+    // 设置div的文本内容为从localStorage中获取的值
+    newDiv.textContent = itemValue;
+
+    // 将新的div添加到body的末尾
+    document.body.appendChild(newDiv);
+} else {
+    // 如果localStorage中没有'add'这个键，或者值为空，可以创建一个提示div
+    var noItemDiv = document.createElement('div');
+    noItemDiv.textContent = 'localStorage中没有找到键为"add"的值';
+    document.body.appendChild(noItemDiv);
+}
+
